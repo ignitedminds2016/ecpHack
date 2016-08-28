33c9ca91c506ce16d2ebcc2890f056a6d75b84cd
onefishgame <fcai_1999@yahoo.com>
2016-08-28 15:21:10 -0700

    Add files via upload
    add file upload feature

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app            = express();
 var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
+var multer  =   require('multer');
 
 // configuration ===========================================
 	
@@ -14,8 +15,6 @@ var db = require('./config/db');
 // connect to our mongoDB database (commented out after you enter in your own credentials)
 connectionAmxData = mongoose.createConnection(db.urlAmxData);
 
-
-
 // get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json 
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
@@ -23,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-f
 
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+
 
 // routes ==================================================
 require('./app/routes/chartRoute')(app);

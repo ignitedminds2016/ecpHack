45c497558cbba07fbda94217d298f576598d246f
dvd <admin@Durgas-MBP.local>
2016-08-29 17:03:51 -0700

    dd

--- random_data_generator.py
+++ random_data_generator.py
@@ -8,21 +8,20 @@ import sys
 def createJson(name,category):
     paymentType=["Amex","Visa","Discover","Master Card", "Other"]
     txnAmt = randrange(10, 100)
-    txnDate=datetime.date(randint(2014,2016), randint(1,12),randint(1,28)).strftime( '%m/%d/%y')
+    txnDate=datetime.date(randint(2014,2016), randint(1,12),randint(1,28)).strftime( '%m/%d/%Y')
     txnZip=randrange( 85020,85030);
     txnMethod= random.choice(paymentType);
-    json='{"merchant_name":'+name+',"category":'+category+',"payment_type":'+txnMethod+',"zipcode":'+str(txnZip)+',"transaction_amount":'+str(txnAmt)+',"date_posted":'+txnDate+'}'
+    json='{"merchant_name":"'+name+'","category":"'+category+'","payment_type":"'+txnMethod+'","zipcode":"'+str(txnZip)+'","transaction_amount":"'+str(txnAmt)+'","date_posted":"'+txnDate+'"},'
     print json 
     return json
 
-if len(sys.argv) < 4 : exit(" Enter name,category,#entries")
-name = sys.argv[1]
-category = sys.argv[2]
-entries= sys.argv[3]
-
-
-if ( name is not None and category is not None and range is not None):
-  for i in range(1,int(entries)):
+fp=open( "data.csv","r")
+for line in fp:
+    values= line.strip("\n").split(",")
+    name=values[1]
+    category=values[0]
+    entries=200
+    for i in range(1,int(entries)):
       createJson( name,category)
 
 

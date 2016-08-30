d4f24fd4cdcdd137e2419a84217886f0bda217e1
Krish Penumarty <krishpenumarty99@gmail.com>
2016-08-29 17:18:06 -0700

    Krish Rocks

--- public/js/Dashboard2.js
+++ public/js/Dashboard2.js
@@ -1,5 +1,5 @@
 function queryparams(){
-var selectedzip = $('#menuselect :selected').text();
+var selectedzip = $('#menuselect1 :selected').text();
 var selectedcat = $('#filterselect :selected').text();
 
 var zip = selectedzip.split(':');
@@ -16,145 +16,145 @@ loaddata(zip[0], cat[0]);
 
 function loaddata(zip,cat){
 queue()
-    .defer(d3.json, "/api/data?state="+zip+"&income_level="+cat)
+    .defer(d3.json, "/api/data?zipcode="+zip+"&category="+cat)
     .await(makeGraphs);
 
 }
 
-function makeGraphs(error, apiData) {
-	
-//Data Transformations
-	var dataSet = apiData;
-	var dateFormat = d3.time.format("%m/%d/%Y");
-	dataSet.forEach(function(d) {
-		d.date_posted = dateFormat.parse(d.date_posted);
-				d.date_posted.setDate(1);
-		d.total_spendings = +d.total_spendings;
-	});
+// function makeGraphs(error, apiData) {
 
-	//Crossfilter instance
-	var cf = crossfilter(dataSet);
+// //Data Transformations
+// 	var dataSet = apiData;
+// 	var dateFormat = d3.time.format("%m/%d/%Y");
+// 	dataSet.forEach(function(d) {
+// 		d.date_posted = dateFormat.parse(d.date_posted);
+// 				d.date_posted.setDate(1);
+// 		d.total_spendings = +d.total_spendings;
+// 	});
 
-	//Define Dimensions
-	var datePosted = cf.dimension(function(d) { return d.date_posted; });
-	var ageGroup = cf.dimension(function(d) { return d.age_group; });
-	var spendingType = cf.dimension(function(d) { return d.spending_type; });
-	var incomeLevel = cf.dimension(function(d) { return d.income_level; });
-	var state = cf.dimension(function(d) { return d.state; });
+// 	//Crossfilter instance
+// 	var cf = crossfilter(dataSet);
 
-
-	//Calculate by group
-	var transactionsByDate = datePosted.group(); 
-	var transactionsByAge = ageGroup.group(); 
-	var transactionsBySpendingType = spendingType.group();
-	var transactionsByIncomeLevel = incomeLevel.group();
-	var stateGroup = state.group();
-
-	var all = cf.groupAll();
-
-	// Total
-	var totalSpendingsState = state.group().reduceSum(function(d) {
-		return d.total_spendings;
-	});
+// 	//Define Dimensions
+// 	var datePosted = cf.dimension(function(d) { return d.date_posted; });
+// 	var ageGroup = cf.dimension(function(d) { return d.age_group; });
+// 	var spendingType = cf.dimension(function(d) { return d.spending_type; });
+// 	var incomeLevel = cf.dimension(function(d) { return d.income_level; });
+// 	var state = cf.dimension(function(d) { return d.state; });
 
 
-	var netTotalSpendings = cf.groupAll().reduceSum(function(d) {return d.total_spendings;});
+// 	//Calculate by group
+// 	var transactionsByDate = datePosted.group();
+// 	var transactionsByAge = ageGroup.group();
+// 	var transactionsBySpendingType = spendingType.group();
+// 	var transactionsByIncomeLevel = incomeLevel.group();
+// 	var stateGroup = state.group();
 
-	//Define range values for data
-	var minDate = datePosted.bottom(1)[0].date_posted;
-	var maxDate = datePosted.top(1)[0].date_posted;
+// 	var all = cf.groupAll();
 
-//console.log(minDate);
-//console.log(maxDate);
-
-    //Charts
-	var dateChart = dc.lineChart("#date-chart");
-	var ageGroupChart = dc.rowChart("#ageGroup-chart");
-	var spendingTypeChart = dc.rowChart("#spending-chart");
-	var incomeLevelChart = dc.rowChart("#income-chart");
-	var totalTransactions = dc.numberDisplay("#total-transactions");
-	var netSpendings = dc.numberDisplay("#net-spendings");
-	var stateSpendings = dc.barChart("#state-spendings");
+// 	// Total
+// 	var totalSpendingsState = state.group().reduceSum(function(d) {
+// 		return d.total_spendings;
+// 	});
 
 
-  selectField = dc.selectMenu('#menuselect')
-        .dimension(state)
-        .group(stateGroup);
-        
-  selectField1 = dc.selectMenu('#filterselect')
-        .dimension(incomeLevel)
-        .group(transactionsByIncomeLevel);
-         
+// 	var netTotalSpendings = cf.groupAll().reduceSum(function(d) {return d.total_spendings;});
 
-       dc.dataCount("#row-selection")
-        .dimension(cf)
-        .group(all);
+// 	//Define range values for data
+// 	var minDate = datePosted.bottom(1)[0].date_posted;
+// 	var maxDate = datePosted.top(1)[0].date_posted;
 
+// //console.log(minDate);
+// //console.log(maxDate);
 
-	totalTransactions
-		.formatNumber(d3.format("d"))
-		.valueAccessor(function(d){return d; })
-		.group(all);
-
-	netSpendings
-		.formatNumber(d3.format("d"))
-		.valueAccessor(function(d){return d; })
-		.group(netTotalSpendings)
-		.formatNumber(d3.format(".4s"));
-
-	dateChart
-		.height(220)
-		.margins({top: 10, right: 50, bottom: 30, left: 50})
-		.dimension(datePosted)
-		.group(transactionsByDate)
-		.renderArea(true)
-		.transitionDuration(500)
-		.x(d3.time.scale().domain([minDate, maxDate]))
-		.elasticY(true)
-		.renderHorizontalGridLines(true)
-    	.renderVerticalGridLines(true)
-		.xAxisLabel("Year")
-		.yAxis().ticks(6);
-
-	spendingTypeChart
-        .height(220)
-        .dimension(spendingType)
-        .group(transactionsBySpendingType)
-        .elasticX(true)
-        .xAxis().ticks(5);
-
-	incomeLevelChart
-		//.width(300)
-		.height(220)
-        .dimension(incomeLevel)
-        .group(transactionsByIncomeLevel)
-        .xAxis().ticks(4);
-
-	ageGroupChart
-		.height(220)
-        .dimension(ageGroup)
-        .group(transactionsByAge)
-        .xAxis().ticks(4);
+//     //Charts
+// 	var dateChart = dc.lineChart("#date-chart");
+// 	var ageGroupChart = dc.rowChart("#ageGroup-chart");
+// 	var spendingTypeChart = dc.rowChart("#spending-chart");
+// 	var incomeLevelChart = dc.rowChart("#income-chart");
+// 	var totalTransactions = dc.numberDisplay("#total-transactions");
+// 	var netSpendings = dc.numberDisplay("#net-spendings");
+// 	var stateSpendings = dc.barChart("#state-spendings");
 
 
-    stateSpendings
-        .height(220)
-        .transitionDuration(1000)
-        .dimension(state)
-        .group(totalSpendingsState)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
-        .centerBar(false)
-        .gap(5)
-        .elasticY(true)
-        .x(d3.scale.ordinal().domain(state))
-        .xUnits(dc.units.ordinal)
-        .renderHorizontalGridLines(true)
-        .renderVerticalGridLines(true)
-        .ordering(function(d){return d.value;})
-        .yAxis().tickFormat(d3.format("s"));
+//   selectField = dc.selectMenu('#menuselect')
+//         .dimension(state)
+//         .group(stateGroup);
+
+//   selectField1 = dc.selectMenu('#filterselect')
+//         .dimension(incomeLevel)
+//         .group(transactionsByIncomeLevel);
+
+
+//        dc.dataCount("#row-selection")
+//         .dimension(cf)
+//         .group(all);
+
+
+// 	totalTransactions
+// 		.formatNumber(d3.format("d"))
+// 		.valueAccessor(function(d){return d; })
+// 		.group(all);
+
+// 	netSpendings
+// 		.formatNumber(d3.format("d"))
+// 		.valueAccessor(function(d){return d; })
+// 		.group(netTotalSpendings)
+// 		.formatNumber(d3.format(".4s"));
+
+// 	dateChart
+// 		.height(220)
+// 		.margins({top: 10, right: 50, bottom: 30, left: 50})
+// 		.dimension(datePosted)
+// 		.group(transactionsByDate)
+// 		.renderArea(true)
+// 		.transitionDuration(500)
+// 		.x(d3.time.scale().domain([minDate, maxDate]))
+// 		.elasticY(true)
+// 		.renderHorizontalGridLines(true)
+//     	.renderVerticalGridLines(true)
+// 		.xAxisLabel("Year")
+// 		.yAxis().ticks(6);
+
+// 	spendingTypeChart
+//         .height(220)
+//         .dimension(spendingType)
+//         .group(transactionsBySpendingType)
+//         .elasticX(true)
+//         .xAxis().ticks(5);
+
+// 	incomeLevelChart
+// 		//.width(300)
+// 		.height(220)
+//         .dimension(incomeLevel)
+//         .group(transactionsByIncomeLevel)
+//         .xAxis().ticks(4);
+
+// 	ageGroupChart
+// 		.height(220)
+//         .dimension(ageGroup)
+//         .group(transactionsByAge)
+//         .xAxis().ticks(4);
+
+
+//     stateSpendings
+//         .height(220)
+//         .transitionDuration(1000)
+//         .dimension(state)
+//         .group(totalSpendingsState)
+//         .margins({top: 10, right: 50, bottom: 30, left: 50})
+//         .centerBar(false)
+//         .gap(5)
+//         .elasticY(true)
+//         .x(d3.scale.ordinal().domain(state))
+//         .xUnits(dc.units.ordinal)
+//         .renderHorizontalGridLines(true)
+//         .renderVerticalGridLines(true)
+//         .ordering(function(d){return d.value;})
+//         .yAxis().tickFormat(d3.format("s"));
 
 
 
-    dc.renderAll();
+//     dc.renderAll();
 
-};
+// };
